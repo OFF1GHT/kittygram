20d859307f9a7357a6306e2e5b31dab41a18393e
Семенова Дарья <kotik.d31sha@yandex.ru>
2023-08-29 18:06:02 +0300

    edit35

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -59,9 +59,11 @@ WSGI_APPLICATION = 'kittygram_backend.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'kittygram',
-        'USER': 'kittygram_user',
-        'PASSWORD': 'kittygram_password',
+        'NAME': os.getenv('kittygram', 'django'),
+        'USER': os.getenv('POSTGRES_USER', 'django'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD', ''),
+        'HOST': os.getenv('DB_HOST', ''),
+        'PORT': os.getenv('DB_PORT', 5432),
     }
 }
 
