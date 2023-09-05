87af5134c68607050ec9e5b71062a642d0586879
Семенова Дарья <kotik.d31sha@yandex.ru>
2023-09-05 15:10:23 +0300

    Edits2

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -8,7 +8,7 @@ SECRET_KEY = os.getenv('SECRET_KEY', 'kittygram_secret')
 
 DEBUG = bool(os.getenv('DEBUG', 'False'))
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '127.0.0.1, localhost').split(',')
 
 INSTALLED_APPS = [
     'django.contrib.admin',
